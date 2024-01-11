84a458160e47ba8468a78445cf05a0a9e59bfbc5
Андрей Мац <amats6655@gmail.com>
2024-01-11 19:22:02 +0300

    💥 NEW: Add new DTO

---
+++ PrintStatus.BLL/DTO/NewPrinterDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace PrintStatus.BLL;
+
+public class NewPrinterDTO
+{
+	public string Title { get; set; }
+	public string IpAddress { get; set; }
+	public int LocationId { get; set; }
+	public int UserProfileId {get; set;}
+}
---
+++ PrintStatus.BLL/DTO/NewUserDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace PrintStatus.BLL;
+
+public class NewUserDTO
+{
+	public required string userName {get; set;}
+	public required string password {get; set;}
+}
