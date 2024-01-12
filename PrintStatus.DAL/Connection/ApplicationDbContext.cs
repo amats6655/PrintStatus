3705ea728d199ef928894ed29a3d4584eb22e2d0
Андрей Мац <amats6655@gmail.com>
2024-01-12 18:01:45 +0300

    👌 IMPROVE: Указать способ удаления связных данных в таблицах

--- PrintStatus.DAL/Connection/ApplicationDbContext.cs
+++ PrintStatus.DAL/Connection/ApplicationDbContext.cs
@@ -6,32 +6,49 @@ using PrintStatus.DOM.Models;
 
 namespace PrintStatus.DAL.Connection
 {
-    public class ApplicationDbContext : IdentityDbContext
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.TrackAll;
-        }
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+			ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.TrackAll;
+		}
 
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
 
-            builder.Entity<UserProfile>()
-                .HasOne<IdentityUser>()
-                .WithOne()
-                .HasForeignKey<UserProfile>(up => up.IdentityId);
-        }
+			builder.Entity<UserProfile>()
+				.HasOne<IdentityUser>()
+				.WithOne()
+				.HasForeignKey<UserProfile>(up => up.IdentityId)
+				.OnDelete(DeleteBehavior.Cascade);
+			builder.Entity<PrintModel>()
+				.HasMany(p => p.Printers)
+				.WithOne(p => p.PrintModel)
+				.OnDelete(DeleteBehavior.Restrict);
+			builder.Entity<Location>()
+				.HasMany(p => p.Printers)
+				.WithOne(p => p.Location)
+				.OnDelete(DeleteBehavior.Restrict);
+			builder.Entity<BasePrinter>()
+				.HasMany(p => p.Histories)
+				.WithOne(p => p.Printer)
+				.OnDelete(DeleteBehavior.Cascade);
+			builder.Entity<Oid>()
+				.HasMany(p => p.Histories)
+				.WithOne(p => p.Oid)
+				.OnDelete(DeleteBehavior.Cascade);
+			
+		}
 
-        public DbSet<AuditLog> AuditLogs { get; set; }
-        public DbSet<BasePrinter> BasePrinters { get; set; } = null!;
-        public DbSet<History> Histories { get; set; }
-        public DbSet<Location> Locations { get; set; }
-        public DbSet<PrintModel> PrintModels { get; set; }
-        public DbSet<Oid> Oids { get; set; }
-        public DbSet<UserProfile> UserProfiles { get; set; }
+		public DbSet<BasePrinter> BasePrinters { get; set; } = null!;
+		public DbSet<History> Histories { get; set; }
+		public DbSet<Location> Locations { get; set; }
+		public DbSet<PrintModel> PrintModels { get; set; }
+		public DbSet<Oid> Oids { get; set; }
+		public DbSet<UserProfile> UserProfiles { get; set; }
 
 
-    }
+	}
 }
