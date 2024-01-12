90da44b6ed19341b1dfed6df6658a8e145814992
Андрей Мац <amats6655@gmail.com>
2024-01-12 17:57:57 +0300

    💥 NEW: Добавить поиск по названию местоположения

--- PrintStatus.DAL/Repositories/LocationRepository.cs
+++ PrintStatus.DAL/Repositories/LocationRepository.cs
@@ -5,91 +5,99 @@ using PrintStatus.DOM.Models;
 
 namespace PrintStatus.DAL.Repositories
 {
-    public class LocationRepository : ILocationRepository
-    {
-        private readonly ApplicationDbContext _context;
-        public LocationRepository(ApplicationDbContext context)
-        {
-            _context = context;
-        }
-        public async Task<Location> AddAsync(Location location)
-        {
-            ArgumentNullException.ThrowIfNull(location);
-            try
-            {
-                _context.Add(location);
-                await _context.SaveChangesAsync();
-                return location;
-            }
-            catch (Exception ex)
-            {
-                //TODO Добавить обработчик ошибок
-                Console.WriteLine(ex.Message);
-                return null;
-            }
+	public class LocationRepository : ILocationRepository
+	{
+		private readonly ApplicationDbContext _context;
+		public LocationRepository(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+		public async Task<Location> AddAsync(Location location)
+		{
+			ArgumentNullException.ThrowIfNull(location);
+			try
+			{
+				_context.Add(location);
+				await _context.SaveChangesAsync();
+				return location;
+			}
+			catch (Exception ex)
+			{
+				//TODO Добавить обработчик ошибок
+				Console.WriteLine(ex.Message);
+				return null;
+			}
 
-        }
+		}
 
-        public async Task<bool> DeleteAsync(Location location)
-        {
-            ArgumentNullException.ThrowIfNull(location);
-            try
-            {
-                _context.Remove(location);
-                await _context.SaveChangesAsync();
-                return true;
-            }
-            catch (Exception ex)
-            {
-                //TODO Добавить обработчик ошибок
-                Console.WriteLine(ex.Message);
-                return false;
-            }
-        }
+		public async Task<bool> DeleteAsync(Location location)
+		{
+			ArgumentNullException.ThrowIfNull(location);
+			try
+			{
+				_context.Remove(location);
+				await _context.SaveChangesAsync();
+				return true;
+			}
+			catch (Exception ex)
+			{
+				//TODO Добавить обработчик ошибок
+				Console.WriteLine(ex.Message);
+				return false;
+			}
+		}
 
-        public async Task<IEnumerable<Location>> GetAllAsync()
-        {
-            try
-            {
-                return await _context.Locations.ToListAsync();
-            }
-            catch (Exception ex)
-            {
-                //TODO Добавить обработчик ошибок
-                Console.WriteLine(ex.Message);
-                return Enumerable.Empty<Location>();
-            }
-        }
+		public async Task<IEnumerable<Location>> GetAllAsync()
+		{
+			try
+			{
+				return await _context.Locations.ToListAsync();
+			}
+			catch (Exception ex)
+			{
+				//TODO Добавить обработчик ошибок
+				Console.WriteLine(ex.Message);
+				return Enumerable.Empty<Location>();
+			}
+		}
 
-        public async Task<Location> GetByIdAsync(int id)
-        {
-            try
-            {
-                return await _context.Locations.FindAsync(id);
-            }
-            catch (Exception ex)
-            {
-                //TODO Добавить обработчик ошибок
-                Console.WriteLine(ex.Message);
-                return null;
-            }
-        }
+		public async Task<Location> GetByIdAsync(int id)
+		{
+			try
+			{
+				return await _context.Locations.FindAsync(id);
+			}
+			catch (Exception ex)
+			{
+				//TODO Добавить обработчик ошибок
+				Console.WriteLine(ex.Message);
+				return null;
+			}
+		}
 
-        public async Task<Location> UpdateAsync(Location location)
-        {
-            ArgumentNullException.ThrowIfNull(location);
-            try
-            {
-                _context.Update(location);
-                await _context.SaveChangesAsync();
-                return location;
-            }
-            catch (Exception ex)
-            {
-                //TODO Добавить обработчик ошибок
-                Console.WriteLine(ex.Message);
-                return null;
-            }
-        }
-    }
+		public async Task<Location> GetByTitle(string title)
+		{
+			ArgumentException.ThrowIfNullOrEmpty(title);
+			return await _context.Locations
+									.Where(l => l.Title.Equals(title))
+									.FirstOrDefaultAsync();
+		}
+
+		public async Task<Location> UpdateAsync(Location location)
+		{
+			ArgumentNullException.ThrowIfNull(location);
+			try
+			{
+				_context.Update(location);
+				await _context.SaveChangesAsync();
+				return location;
+			}
+			catch (Exception ex)
+			{
+				//TODO Добавить обработчик ошибок
+				Console.WriteLine(ex.Message);
+				return null;
+			}
+		}
+	}
 }
--- PrintStatus.DOM/Interfaces/ILocationRepository.cs
+++ PrintStatus.DOM/Interfaces/ILocationRepository.cs
@@ -2,12 +2,13 @@
 
 namespace PrintStatus.DOM.Interfaces
 {
-    public interface ILocationRepository
-    {
-        Task<Location> GetByIdAsync(int id);
-        Task<IEnumerable<Location>> GetAllAsync();
-        Task<Location> AddAsync(Location location);
-        Task<bool> DeleteAsync(Location location);
-        Task<Location> UpdateAsync(Location location);
-    }
+	public interface ILocationRepository
+	{
+		Task<Location> GetByIdAsync(int id);
+		Task<IEnumerable<Location>> GetAllAsync();
+		Task<Location> AddAsync(Location location);
+		Task<bool> DeleteAsync(Location location);
+		Task<Location> UpdateAsync(Location location);
+		Task<Location> GetByTitle(string Title);
+	}
 }
