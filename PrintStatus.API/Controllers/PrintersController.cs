a9480f169080f728d183418c3cab38b165f12e2e
Андрей Мац <amats6655@gmail.com>
2024-01-12 18:07:03 +0300

    👌 IMPROVE: Добавить получения Id пользователя для тестового обращения к сервису

--- PrintStatus.API/Controllers/PrintersController.cs
+++ PrintStatus.API/Controllers/PrintersController.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Security.Claims;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using PrintStatus.BLL;
 using PrintStatus.BLL.DTO;
@@ -8,56 +9,58 @@ using PrintStatus.BLL.Interfaces;
 
 namespace PrintStatus.API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class PrintersController : ControllerBase
+	[Route("api/[controller]")]
+	[ApiController]
+	public class PrintersController : ControllerBase
 
-    {
-        private readonly IBasePrinterManagementService _printService;
-        public PrintersController(IBasePrinterManagementService printerManagementService)
-        {
-            _printService = printerManagementService;
-        }
-        // GET: api/<PrintersController>
-        [HttpGet]
-        public async Task<IEnumerable<PrinterDTO>> Get()
-        {
-            return await _printService.GetAllAsync();
-        }
+	{
+		private readonly IBasePrinterManagementService _printService;
+		public PrintersController(IBasePrinterManagementService printerManagementService)
+		{
+			_printService = printerManagementService;
+		}
+		// GET: api/<PrintersController>
+		[HttpGet]
+		public async Task<IEnumerable<PrinterDTO>> Get()
+		{
+			return await _printService.GetAllAsync();
+		}
 
-        // GET api/<PrintersController>/5
-        [HttpGet("{id}")]
-        public async Task<PrinterDTO> Get(int id)
-        {
-            var result = await _printService.GetByIdAsync(id);
-            string json = JsonSerializer.Serialize(result);
-            return result;
-        }
+		// GET api/<PrintersController>/5
+		[HttpGet("{id}")]
+		public async Task<PrinterDTO> Get(int id)
+		{
+			var userId = HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+			var result = await _printService.GetByIdAsync(id, userId);
+			return result;
+		}
 
-        // POST api/<PrintersController>
-        [HttpPost]
-        public async Task<StatusCodeResult> Post([FromBody] NewPrinterDTO value)
-        {
-            var result = await _printService.AddAsync(value.Title, value.IpAddress, value.LocationId, value.UserProfileId);
-            if (result != null)
-            {
-                return Ok();
-            }
-            return NoContent();
-        }
+		// POST api/<PrintersController>
+		[HttpPost]
+		public async Task<StatusCodeResult> Post([FromBody] NewPrinterDTO value)
+		{
+			var userId = HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+			var result = await _printService.AddAsync(value.Title, value.IpAddress, value.LocationId, userId);
+			if (result != null)
+			{
+				return Ok();
+			}
+			return NoContent();
+		}
 
-        // PUT api/<PrintersController>/5
-        [HttpPut("{id}")]
-        public void Put()
-        {
+		// PUT api/<PrintersController>/5
+		[HttpPut("{id}")]
+		public void Put()
+		{
 
-        }
+		}
 
-        // DELETE api/<PrintersController>/5
-        [HttpDelete("{id}")]
-        public async Task<bool> Delete(int id)
-        {
-            return await _printService.DeleteAsync(id, 2);
-        }
-    }
+		// DELETE api/<PrintersController>/5
+		[HttpDelete("{id}")]
+		public async Task<bool> Delete(int id)
+		{
+			var userId = HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+			return await _printService.DeleteAsync(id, userId);
+		}
+	}
 }
