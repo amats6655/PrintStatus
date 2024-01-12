24c8318b6b30381b4f7aa11ed9d0f4053af8f78d
Андрей Мац <amats6655@gmail.com>
2024-01-12 18:02:34 +0300

    💥 NEW: Добавить DTO для модели принтера

---
+++ PrintStatus.BLL/DTO/PrintModelDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace PrintStatus.BLL;
+
+public class PrintModelDTO
+{
+	public int Id { get; set; }
+	public string Title { get; set; }
+	public bool IsColor { get; set; }
+	public string ConsumableCalcType { get; set; }
+}
