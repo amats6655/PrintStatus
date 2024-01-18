ee48dab2bb0fd5cfe757597a50912e93d749bcb0
Андрей Мац <amats6655@gmail.com>
2024-01-18 19:59:21 +0300

    👌 IMPROVE: Упаковать возвращаемые ответы в IServiceResult. Добавить проверки получаемых значений

--- PrintStatus.BLL/Services/HistoryManagementService.cs
+++ PrintStatus.BLL/Services/HistoryManagementService.cs
@@ -1,23 +1,49 @@
-﻿using PrintStatus.BLL.Interfaces;
+﻿using PrintStatus.BLL.DTO;
+using PrintStatus.BLL.Interfaces;
+using PrintStatus.DOM.Interfaces;
 using PrintStatus.DOM.Models;
 
 namespace PrintStatus.BLL.Services
 {
-    public class HistoryManagementService : IHistoryManagementService
-    {
-        public Task<bool> AddHistory(int pritnerId, int OidId, string Value)
-        {
-            throw new NotImplementedException();
-        }
+	public class HistoryManagementService : IHistoryManagementService
+	{
+		private readonly IHistoryRepository _historyRepo;
+		public HistoryManagementService(IHistoryRepository historyRepo)
+		{
+			_historyRepo = historyRepo;
+		}
 
-        public Task<IEnumerable<History>> GetAllByPrinterId(int PrinterId)
-        {
-            throw new NotImplementedException();
-        }
+		public async Task<IServiceResult<bool>> AddHistory(History history)
+		{
+			if (history == null) return ServiceResult<bool>.Failure("Неверный идентификатор истории");
+			history.Date = DateTime.UtcNow;
+			var resultAddHistory = await _historyRepo.AddAsync(history);
+			if (!resultAddHistory.IsSuccess) return ServiceResult<bool>.Failure(resultAddHistory.Message);
+			return ServiceResult<bool>.Success(true, "Добавлена запись в историю");
+		}
 
-        public Task<History> GetById(int id)
-        {
-            throw new NotImplementedException();
-        }
-    }
+		public async Task<IServiceResult<IEnumerable<History>>> GetAllByPrinterId(int printerId)
+		{
+			if (printerId <= 0) return ServiceResult<IEnumerable<History>>.Failure("Неверный идентификатор принтера");
+			var histories = await _historyRepo.GetPrinterHistoriesAsync(printerId);
+			if (!histories.IsSuccess) return ServiceResult<IEnumerable<History>>.Failure(histories.Message);
+			return ServiceResult<IEnumerable<History>>.Success(histories.Data, "История получена");
+		}
+
+		public async Task<IServiceResult<IEnumerable<History>>> GetAllHistories()
+		{
+			var histories = await _historyRepo.GetAllAsync();
+			if (!histories.IsSuccess) return ServiceResult<IEnumerable<History>>.Failure(histories.Message);
+			return ServiceResult<IEnumerable<History>>.Success(histories.Data, "История получена");
+		}
+
+		public async Task<IServiceResult<History>> GetById(int id)
+		{
+			if (id <= 0) return ServiceResult<History>.Failure("Неверный идентификатор истории");
+			var history = await _historyRepo.GetByIdAsync(id);
+			if (!history.IsSuccess) return ServiceResult<History>.Failure(history.Message);
+			return ServiceResult<History>.Success(history.Data, "История получена");
+		}
+
+	}
 }
--- PrintStatus.BLL/Services/LocationManagementService.cs
+++ PrintStatus.BLL/Services/LocationManagementService.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using PrintStatus.BLL.DTO;
 using PrintStatus.BLL.Interfaces;
 using PrintStatus.DOM.Interfaces;
 using PrintStatus.DOM.Models;
@@ -9,74 +10,77 @@ namespace PrintStatus.BLL.Services
 	{
 		private readonly ILocationRepository _locationRepo;
 		private readonly IMapper _mapper;
+		private readonly IAccountService _accountService;
 
 		public LocationManagementService
 										(
 											ILocationRepository locationRepo,
-											IMapper mapper
+											IMapper mapper,
+											IAccountService accountService
 										)
 		{
 			_mapper = mapper;
 			_locationRepo = locationRepo;
+			_accountService = accountService;
 		}
 
-		public async Task<LocationDTO> AddAsync(LocationDTO location)
+		public async Task<IServiceResult<LocationDTO>> AddAsync(LocationDTO location)
 		{
-			ArgumentNullException.ThrowIfNull(location);
-			var locationExists = await _locationRepo.GetByTitle(location.Title);
-			if (locationExists == null)
-			{
-				var newLocation = _mapper.Map<Location>(location);
-				var resultAdd = await _locationRepo.AddAsync(newLocation);
-				return _mapper.Map<LocationDTO>(resultAdd);
-			}
-			return _mapper.Map<LocationDTO>(locationExists);
+			if (location == null) return ServiceResult<LocationDTO>.Failure("Неверный идентификатор местоположения");
+			var locationExist = await _locationRepo.GetByTitleAsync(location.Title);
+			if (locationExist.Errors.Any()) return ServiceResult<LocationDTO>.Failure(locationExist.Message);
+			if (locationExist.IsSuccess) return ServiceResult<LocationDTO>.Failure("Такое местоположение уже существует");
+			var newLocation = _mapper.Map<Location>(location);
+			var addLocationResult = await _locationRepo.AddAsync(newLocation);
+			if (!addLocationResult.IsSuccess) return ServiceResult<LocationDTO>.Failure(addLocationResult.Message);
+			location = _mapper.Map<LocationDTO>(addLocationResult.Data);
+			return ServiceResult<LocationDTO>.Success(location, addLocationResult.Message);
+
 		}
 
-		public async Task<bool> DeleteAsync(int id)
+		public async Task<IServiceResult<bool>> DeleteAsync(int id)
 		{
-			if (id != 0)
-			{
-				var location = await _locationRepo.GetByIdAsync(id);
-				if (location != null && !location.Printers.Any())
-				{
-					return await _locationRepo.DeleteAsync(location);
-				}
-			}
-			return false;
+			if (id <= 0) return ServiceResult<bool>.Failure("Неверный идентификатор местоположения");
+			var locationExist = await _locationRepo.GetByIdAsync(id);
+			if (locationExist.Errors.Any()) return ServiceResult<bool>.Failure(locationExist.Message);
+			var resultDelete = await _locationRepo.DeleteAsync(locationExist.Data);
+			if (!resultDelete.IsSuccess) return ServiceResult<bool>.Failure(resultDelete.Message);
+			return ServiceResult<bool>.Success(true, resultDelete.Message);
 		}
 
-		public async Task<IEnumerable<LocationDTO>> GetAllAsync()
+		public async Task<IServiceResult<IEnumerable<LocationDTO>>> GetAllAsync()
 		{
-			var result = new List<LocationDTO>();
 			var locations = await _locationRepo.GetAllAsync();
-
-			foreach (var location in locations) result.Add(_mapper.Map<LocationDTO>(location));
-			return result;
-
+			if (!locations.IsSuccess) return ServiceResult<IEnumerable<LocationDTO>>.Failure(locations.Message);
+			var result = new List<LocationDTO>();
+			foreach (var location in locations.Data)
+			{
+				result.Add(_mapper.Map<LocationDTO>(location));
+			}
+			return ServiceResult<IEnumerable<LocationDTO>>.Success(result, locations.Message);
 		}
 
-		public async Task<LocationDTO> GetByIdAsync(int id)
+		public async Task<IServiceResult<LocationDTO>> GetByIdAsync(int id)
 		{
-			if (id != 0)
-			{
-				var dataLocation = await _locationRepo.GetByIdAsync(id);
-				return _mapper.Map<LocationDTO>(dataLocation);
-			}
-			return null;
+			if (id <= 0) return ServiceResult<LocationDTO>.Failure("Неверный идентификатор местоположения");
+			var location = await _locationRepo.GetByIdAsync(id);
+			if (!location.IsSuccess) return ServiceResult<LocationDTO>.Failure(location.Message);
+			var result = _mapper.Map<LocationDTO>(location);
+			return ServiceResult<LocationDTO>.Success(result, location.Message);
 		}
 
-		public async Task<LocationDTO> UpdateAsync(LocationDTO location)
+		public async Task<IServiceResult<LocationDTO>> UpdateAsync(LocationDTO locationDTO, string identityUserId)
 		{
-			ArgumentException.ThrowIfNullOrEmpty(nameof(location));
-			var locationExist = await _locationRepo.GetByIdAsync(location.Id);
-			if (locationExist != null)
-			{
-				locationExist.Title = location.Title;
-				await _locationRepo.UpdateAsync(locationExist);
-				return location;
-			}
-			return null;
+			if (locationDTO == null) return ServiceResult<LocationDTO>.Failure("Неверный идентификатор местоположения");
+			if (string.IsNullOrEmpty(identityUserId)) return ServiceResult<LocationDTO>.Failure("Неавторизованная операция");
+			var userRoles = await _accountService.GetRolesAsync(identityUserId);
+			if (!userRoles.IsSuccess) return ServiceResult<LocationDTO>.Failure(userRoles.Message);
+			if (!userRoles.Data.Any(r => r.Equals("Администратор"))) return ServiceResult<LocationDTO>.Failure("Недостаточно прав для обновления местоположения");
+			Location location = _mapper.Map<Location>(locationDTO);
+			var resultUpdate = await _locationRepo.UpdateAsync(location);
+			if (!resultUpdate.IsSuccess) return ServiceResult<LocationDTO>.Failure(resultUpdate.Message);
+			locationDTO = _mapper.Map<LocationDTO>(resultUpdate.Data);
+			return ServiceResult<LocationDTO>.Success(locationDTO, resultUpdate.Message);
 		}
 	}
 }
