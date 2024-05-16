42f210e2f2ac8cab572296cb372991b0f7c1ae0f
amats <amats6655@gmail.com>
2024-05-16 23:48:22 +0300

    :fire: удалить неиспользуемый код из SnmpService.cs

--- PrintStatus.BLL/Services/Implementations/SnmpService.cs
+++ PrintStatus.BLL/Services/Implementations/SnmpService.cs
@@ -11,25 +11,9 @@ using Lextm.SharpSnmpLib.Messaging;
 public class SnmpService : ISnmpService
 {
 	private const int PORT = 161;
-	private Variable SerialNumber = new(new ObjectIdentifier("1.3.6.1.2.1.43.5.1.1.17.1"));
-	private Variable Model = new(new ObjectIdentifier("1.3.6.1.2.1.25.3.2.1.3.1"));
+	private readonly Variable _serialNumber = new(new ObjectIdentifier("1.3.6.1.2.1.43.5.1.1.17.1"));
+	private readonly Variable _model = new(new ObjectIdentifier("1.3.6.1.2.1.25.3.2.1.3.1"));
 
-	//public async Task<List<Variable>> GetOidsAsync(string ipAddress, List<Variable> oids)
-	//{
-	//    try
-	//    {
-	//        return await Messenger.GetAsync(VersionCode.V2,
-	//                    new IPEndPoint(IPAddress.Parse(ipAddress), PORT),
-	//                    new OctetString("public"),
-	//                    oids) as List<Variable>;
-	//    }
-	//    catch (Exception ex)
-	//    {
-	//        //TODO Написать обработчик ошибок
-	//        Console.WriteLine(ex.Message);
-	//        return new List<Variable>();
-	//    }
-	//}
 
 	public async Task<IServiceResponse<Dictionary<string, string>>> GetModelAndSerialNumAsync(string ipAddress)
 	{
@@ -47,7 +31,7 @@ public class SnmpService : ISnmpService
 		PingReply pingRes = await ping.SendPingAsync(ipAddress, 100);
 		if (pingRes.Status != IPStatus.Success) return ServiceResponse<Dictionary<string, string>>.Failure("Принтер недоступен");
 		var result = new Dictionary<string, string>();
-		var baseOids = new List<Variable>() { Model, SerialNumber };
+		var baseOids = new List<Variable>() { _model, _serialNumber };
 		try
 		{
 			var value = await Messenger.GetAsync(VersionCode.V2,
@@ -68,7 +52,7 @@ public class SnmpService : ISnmpService
 
 	public async Task<IServiceResponse<IEnumerable<Variable>>> GetOidsAsync(string ipAddress, List<Variable> oids)
 	{
-		if (string.IsNullOrEmpty(ipAddress)) return ServiceResponse<IEnumerable<Variable>>.Failure("Неверный ip адрес");
+		if (string.IsNullOrEmpty(ipAddress)) return ServiceResponse<IEnumerable<Variable>>.Error("Неверный ip адрес");
 		if (oids.Count == 0) return ServiceResponse<IEnumerable<Variable>>.Failure("Пустой список Oid");
 		IPAddress address;
 		try
@@ -77,7 +61,7 @@ public class SnmpService : ISnmpService
 		}
 		catch
 		{
-			return ServiceResponse<IEnumerable<Variable>>.Failure("Неверный формат адреса");
+			return ServiceResponse<IEnumerable<Variable>>.Error("Неверный формат адреса");
 		}
 		using var ping = new Ping();
 		PingReply pingRes = await ping.SendPingAsync(ipAddress, 100);
@@ -89,14 +73,14 @@ public class SnmpService : ISnmpService
 						new IPEndPoint(address, PORT),
 						new OctetString("public"),
 						oids);
-			for (int i = 0; i < values.Count; i++)
+			foreach (var t in values)
 			{
-				result.Add(values[i]);
+				result.Add(t);
 			}
 		}
 		catch (Exception ex)
 		{
-			return ServiceResponse<IEnumerable<Variable>>.Failure(ex.Message);
+			return ServiceResponse<IEnumerable<Variable>>.Error(ex.Message);
 		}
 		if (result.Count == 0) return ServiceResponse<IEnumerable<Variable>>.Failure("Не удалось получить информацию от принтера");
 		return ServiceResponse<IEnumerable<Variable>>.Success(result);
